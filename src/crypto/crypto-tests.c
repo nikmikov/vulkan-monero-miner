6fb04867dcdd123b6328713744852b145a23eadd
Dorenom <dorenomlab@gmail.com>
2018-01-28 11:32:52 +1300

    Fixed skeing 512-256

--- src/crypto/crypto-tests.c
+++ src/crypto/crypto-tests.c
@@ -8,7 +8,7 @@
 #include "crypto/groestl.h"
 #include "crypto/jh256.h"
 #include "crypto/keccak-tiny.h"
-#include "crypto/skein256.h"
+#include "crypto/skein.h"
 #include "crypto/utils.h"
 
 #define UNUSED_ARG(a) ((void)a)
@@ -30,6 +30,7 @@ static const struct test_vector NIST_TEST_VECTORS[] = {
     {.msg = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoij"
             "klmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu",
      .repeat = 0},
+    {.msg = "a", .repeat = 64},
     {.msg = "a", .repeat = 1000000},
     {.msg = "abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmno",
      .repeat = 16777216}};
@@ -39,6 +40,7 @@ static const char *SHA3_256_RESULTS[] = {
     "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a",
     "41c0dba2a9d6240849100376a8235e2c82e1b9998a999e21db32dd97496d3376",
     "916f6061fe879741ca6469b43971dfdb28b1a32dc36cb3254e812be27aad1d18",
+    "043d104b5480439c7acff8831ee195183928d9b7f8fcb0c655a086a87923ffee",
     "5c8875ae474a3634ba4fd55ec85bffd661f32aca75c6d699d0cdcb6c115891c1",
     "ecbbc42cbf296603acb2c6bc0410ef4378bafb24b710357f12df607758b33e2b"};
 
@@ -47,6 +49,7 @@ static const char *KECCAK_256_RESULTS[] = {
     "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470",
     "45d3b367a6904e6e8d502ee04999a7c27647f91fa845d456525fd352ae3d7371",
     "f519747ed599024f3882238e5ab43960132572b7345fbeb9a90769dafd21ad67",
+    "1036d73cc8350b0635393d79759b10488165e792073f84d4462e22edec243b92",
     "fadae6b49f129bbb812be8407b7b2894f34aecf6dbd1f9b0f0c7e9853098fc96",
     "5f313c39963dcf792b5470d4ade9f3a356a3e4021748690a958372e2b06f82a4"};
 
@@ -55,6 +58,7 @@ static const char *BLAKE_256_RESULTS[] = {
     "716f6e863f744b9ac22c97ec7b76ea5f5908bc5b2f67c61510bfc4751384ea7a",
     "adb13cb0da78463d36fcf40def3f291b3f0673e78127bdb70942cdd640b907b4",
     "8f69d890786569cc878e9995a0ebf5e319746482ab56b8184fec5267190e6ade",
+    "84d7f3bbf2cfc3ee940ddb6d25045c6d3f756c4b2077a8128e171d5d165be170",
     "22be6de4aa4214c9403f10598f0a6b0e834570251a13bc27589437f7139a5d44",
     "b2c2ecebdb12cb0193fc18e66e00942a73fff801b4bcd89cc11a7e0e7eea8ae3"};
 
@@ -63,6 +67,7 @@ static const char *SKEIN_256_RESULTS[] = {
     "39ccc4554a8b31853b9de7a1fe638a24cce6b35a55f2431009e18780335d2621",
     "55b077673b26b01842cb2393b2ab4481ce94cdeb1ab56f579f8a2a6937ad5b44",
     "9516ff1e8a8b889294cf6339cea01706a6d8bd582d0c457ced9d740ccffdcf0a",
+    "6b8cd8ac4c67fb6468896693b8f5d3bb54002da20901699233b318bbd10fce85",
     "f1904077d9f219b300c49987d2f3b2f1054fea6338a5a327fda1cb02afe218f8",
     "6e44df5b41110404a558111eb4bbd4b088aef7de2d5833171a9695c091f8b9b1"};
 
@@ -71,6 +76,7 @@ static const char *JH_256_RESULTS[] = {
     "46e64619c18bb0a92a5e87185a47eef83ca747b8fcc8e1412921357e326df434",
     "146105745c7e2dfac6a48a9b1223b111fed3a53c3efbfe8c2b2e8fa9cafd94d4",
     "f432ab29a0a72569376d39c4cbb418081c6e114b7890cdf815ea188b0a9428d9",
+    "05733727efdd236118340ec8f870689c0c9e571d3ff64614cfea082599e56593",
     "c229c3fcdcbe9fd6e935e80746f31dc76f4241fdc092d9893a1960d59ef1b38e",
     "58ffbde520764dfc03b29598acd70655bb2c245a3d73fdd6eb9e1bc221af579b"};
 
@@ -79,6 +85,7 @@ static const char *GROESTL_256_RESULTS[] = {
     "1a52d11d550039be16107f9c58db9ebcc417f16f736adb2502567119f0083467",
     "22c23b160e561f80924d44f2cc5974cd5a1d36f69324211861e63b9b6cb7974c",
     "2538fe0a0ce6e6fee1f5a361c171543bfea6c692e09f160eeb8e10ae97dba4bb",
+    "56e6d76870910b6d4258c6f5fdbee846873f94437d6409ab53922b91ce4afe8c",
     "a43cb4311fb1b53e2b207b1345e4e81c4279cf7afc9531ef10fb9edf4e705daf",
     "5f87f9404c1142b9e701076dd047386162213a896560c1656c62bbfedfbeddb6"};
 
@@ -102,7 +109,7 @@ void do_blake(const void *msg, size_t msg_len, uint8_t *digest)
 
 void do_skein(const void *msg, size_t msg_len, uint8_t *digest)
 {
-  skein256_hash(msg, msg_len, digest);
+  skein_512_256(msg, msg_len * 8, digest);
 }
 
 void do_jh(const void *msg, size_t msg_len, uint8_t *digest)
@@ -140,10 +147,10 @@ bool test_single(const struct test_vector *test, const char *expected,
   char digest_str[DIGEST_LENGTH_BYTES * 2 + 1] = {0};
   bin2hex(digest, DIGEST_LENGTH_BYTES, digest_str);
   if (strncmp(expected, digest_str, DIGEST_LENGTH_BYTES * 2) != 0) {
-    printf(" - FAILED: %s <> %s\n", expected, digest_str);
+    printf(" - FAIL: %s <> %s\n", expected, digest_str);
     return false;
   } else {
-    printf(" + MATCH: %s\n", expected);
+    printf(" + PASS: %s\n", expected);
     return true;
   }
 }
@@ -164,7 +171,7 @@ int main(int argc, char **argv)
   test_hash("SHA-3", SHA3_256_RESULTS, do_sha3);
   test_hash("Keccak", KECCAK_256_RESULTS, do_keccak);
   test_hash("Blake", BLAKE_256_RESULTS, do_blake);
-  test_hash("Skein", SKEIN_256_RESULTS, do_skein);
+  test_hash("Skein-512-256", SKEIN_256_RESULTS, do_skein);
   test_hash("JH", JH_256_RESULTS, do_jh);
   test_hash("Groestl", GROESTL_256_RESULTS, do_groestl);
   assert(false);
--- src/crypto/skein.c
+++ src/crypto/skein.c
@@ -1,56 +1,78 @@
 #include "crypto/skein.h"
 
+#include "crypto/utils.h"
 #include <string.h>
 
 /* tweak word T[1]: flag bit definition(s) */
-#define SKEIN_T1_BIT(BIT)      ((BIT)-64)
-#define SKEIN_T1_POS_FIRST     SKEIN_T1_BIT(126) /* bits 126 : first block flag */
-#define SKEIN_T1_POS_FINAL     SKEIN_T1_BIT(127) /* bit 127 : final block flag */
-#define SKEIN_T1_POS_BIT_PAD   SKEIN_T1_BIT(119) /* bit 119 : partial final input byte */
-#define SKEIN_T1_POS_BLK_TYPE  SKEIN_T1_BIT(120) /* bits 120..125: type field */
+#define SKEIN_T1_BIT(BIT) ((BIT)-64)
+#define SKEIN_T1_POS_FIRST SKEIN_T1_BIT(126) /* bits 126 : first block flag */
+#define SKEIN_T1_POS_FINAL SKEIN_T1_BIT(127) /* bit 127 : final block flag */
+#define SKEIN_T1_POS_BIT_PAD                                                   \
+  SKEIN_T1_BIT(119) /* bit 119: partial final input byte */
+#define SKEIN_T1_POS_BLK_TYPE SKEIN_T1_BIT(120) /* bits 120..125: type field   \
+                                                 */
 
 /* tweak word T[1]: flag bit definition(s) */
 #define SKEIN_T1_FLAG_FIRST (((uint64_t)1) << SKEIN_T1_POS_FIRST)
 #define SKEIN_T1_FLAG_FINAL (((uint64_t)1) << SKEIN_T1_POS_FINAL)
 #define SKEIN_T1_FLAG_BIT_PAD (((uint64_t)1) << SKEIN_T1_POS_BIT_PAD)
 
-#define SKEIN_T1_BLK_TYPE(T) (((uint64_t) (SKEIN_BLK_TYPE_##T)) << SKEIN_T1_POS_BLK_TYPE)
+#define SKEIN_T1_BLK_TYPE(T)                                                   \
+  (((uint64_t)(SKEIN_BLK_TYPE_##T)) << SKEIN_T1_POS_BLK_TYPE)
 
 /* message processing */
-#define SKEIN_BLK_TYPE_MSG      (48)
-#define SKEIN_T1_BLK_TYPE_MSG   SKEIN_T1_BLK_TYPE(MSG)
- /* output stage */
-#define SKEIN_BLK_TYPE_OUT      (63)
-#define SKEIN_T1_BLK_TYPE_OUT   SKEIN_T1_BLK_TYPE(OUT)
-#define SKEIN_T1_BLK_TYPE_OUT_FINAL (SKEIN_T1_BLK_TYPE_OUT | SKEIN_T1_FLAG_FINAL)
+#define SKEIN_BLK_TYPE_MSG (48)
+#define SKEIN_T1_BLK_TYPE_MSG SKEIN_T1_BLK_TYPE(MSG)
+/* output stage */
+#define SKEIN_BLK_TYPE_OUT (63)
+#define SKEIN_T1_BLK_TYPE_OUT SKEIN_T1_BLK_TYPE(OUT)
+#define SKEIN_T1_BLK_TYPE_OUT_FINAL                                            \
+  (SKEIN_T1_BLK_TYPE_OUT | SKEIN_T1_FLAG_FINAL)
 
 #define RotL_64(x, N) (((x) << (N)) | ((x) >> (64 - (N))))
 
 enum {
   /* Skein_256 round rotation constants */
-  R_256_0_0 = 5,
-  R_256_0_1 = 56,
-  R_256_1_0 = 36,
-  R_256_1_1 = 28,
-  R_256_2_0 = 13,
-  R_256_2_1 = 46,
-  R_256_3_0 = 58,
-  R_256_3_1 = 44,
-  R_256_4_0 = 26,
-  R_256_4_1 = 20,
-  R_256_5_0 = 53,
-  R_256_5_1 = 35,
-  R_256_6_0 = 11,
-  R_256_6_1 = 42,
-  R_256_7_0 = 59,
-  R_256_7_1 = 50
+  R_512_0_0 = 46,
+  R_512_0_1 = 36,
+  R_512_0_2 = 19,
+  R_512_0_3 = 37,
+  R_512_1_0 = 33,
+  R_512_1_1 = 27,
+  R_512_1_2 = 14,
+  R_512_1_3 = 42,
+  R_512_2_0 = 17,
+  R_512_2_1 = 49,
+  R_512_2_2 = 36,
+  R_512_2_3 = 39,
+  R_512_3_0 = 44,
+  R_512_3_1 = 9,
+  R_512_3_2 = 54,
+  R_512_3_3 = 56,
+  R_512_4_0 = 39,
+  R_512_4_1 = 30,
+  R_512_4_2 = 34,
+  R_512_4_3 = 24,
+  R_512_5_0 = 13,
+  R_512_5_1 = 50,
+  R_512_5_2 = 10,
+  R_512_5_3 = 17,
+  R_512_6_0 = 25,
+  R_512_6_1 = 29,
+  R_512_6_2 = 39,
+  R_512_6_3 = 43,
+  R_512_7_0 = 8,
+  R_512_7_1 = 35,
+  R_512_7_2 = 56,
+  R_512_7_3 = 22
 };
 
 /*  process single block  */
-void skein_256_process_block(struct skein_256_state *state, uint64_t *data)
+void skein_512_process_block(struct skein_512_state *state, uint64_t *data,
+                             uint64_t byteCntAdd)
 {
-  static const size_t WCNT = SKEIN_256_STATE_WORDS;
-  static const size_t RCNT = SKEIN_256_ROUNDS_TOTAL / 8;
+  static const size_t WCNT = SKEIN_512_STATE_WORDS;
+  static const size_t RCNT = SKEIN_512_ROUNDS_TOTAL / 8;
 
   /* key schedule words : chaining vars + tweak + "rotation"*/
   uint64_t kw[WCNT + 4 + RCNT * 2];
@@ -58,76 +80,93 @@ void skein_256_process_block(struct skein_256_state *state, uint64_t *data)
   uint64_t *ts = kw;
 
   /* local copy of context vars, for speed */
-  uint64_t X0, X1, X2, X3;
+  uint64_t X0, X1, X2, X3, X4, X5, X6, X7;
 
-  ts[0] = state->T[0];
+  ts[0] = state->T[0] + byteCntAdd;
   ts[1] = state->T[1];
 
   /* this implementation only supports 2**64 input bytes (no carry out here) */
-
-  /* update processed length */
-  ts[0] += SKEIN_256_BLOCK_SIZE;
 
   /* precompute the key schedule for this block */
   ks[0] = state->chaining[0];
   ks[1] = state->chaining[1];
   ks[2] = state->chaining[2];
   ks[3] = state->chaining[3];
-  ks[4] = ks[0] ^ ks[1] ^ ks[2] ^ ks[3] ^ SKEIN_KS_PARITY;
+  ks[4] = state->chaining[4];
+  ks[5] = state->chaining[5];
+  ks[6] = state->chaining[6];
+  ks[7] = state->chaining[7];
+  ks[8] = ks[0] ^ ks[1] ^ ks[2] ^ ks[3] ^ ks[4] ^ ks[5] ^ ks[6] ^ ks[7] ^
+          SKEIN_KS_PARITY;
 
   ts[2] = ts[0] ^ ts[1];
 
   /* do the first full key injection */
   X0 = data[0] + ks[0];
-  X1 = data[1] + ks[1] + ts[0];
-  X2 = data[2] + ks[2] + ts[1];
+  X1 = data[1] + ks[1];
+  X2 = data[2] + ks[2];
   X3 = data[3] + ks[3];
+  X4 = data[4] + ks[4];
+  X5 = data[5] + ks[5] + ts[0];
+  X6 = data[6] + ks[6] + ts[1];
+  X7 = data[7] + ks[7];
 
-#define R256(p0, p1, p2, p3, ROT, rNum)                                        \
+#define R512(p0, p1, p2, p3, p4, p5, p6, p7, ROT, rNum)                        \
   X##p0 += X##p1;                                                              \
   X##p1 = RotL_64(X##p1, ROT##_0);                                             \
   X##p1 ^= X##p0;                                                              \
   X##p2 += X##p3;                                                              \
   X##p3 = RotL_64(X##p3, ROT##_1);                                             \
-  X##p3 ^= X##p2;
+  X##p3 ^= X##p2;                                                              \
+  X##p4 += X##p5;                                                              \
+  X##p5 = RotL_64(X##p5, ROT##_2);                                             \
+  X##p5 ^= X##p4;                                                              \
+  X##p6 += X##p7;                                                              \
+  X##p7 = RotL_64(X##p7, ROT##_3);                                             \
+  X##p7 ^= X##p6;
 
-#define I256(R)                                                                \
-  X0 += ks[r + (R) + 0]; /* inject the key schedule value */                   \
-  X1 += ks[r + (R) + 1] + ts[r + (R) + 0];                                     \
-  X2 += ks[r + (R) + 2] + ts[r + (R) + 1];                                     \
-  X3 += ks[r + (R) + 3] + r + (R);                                             \
-  ks[r + (R) + 4] = ks[r + (R)-1]; /* rotate key schedule */                   \
-  ts[r + (R) + 2] = ts[r + (R)-1];
+#define I512(R)                                                                \
+  X0 += ks[((R) + 1) % 9]; /* inject the key schedule value */                 \
+  X1 += ks[((R) + 2) % 9];                                                     \
+  X2 += ks[((R) + 3) % 9];                                                     \
+  X3 += ks[((R) + 4) % 9];                                                     \
+  X4 += ks[((R) + 5) % 9];                                                     \
+  X5 += ks[((R) + 6) % 9] + ts[((R) + 1) % 3];                                 \
+  X6 += ks[((R) + 7) % 9] + ts[((R) + 2) % 3];                                 \
+  X7 += ks[((R) + 8) % 9] + (R) + 1;
 
-#define R256_8_rounds(R)                                                       \
-  R256(0, 1, 2, 3, R_256_0, 8 * (R) + 1);                                      \
-  R256(0, 3, 2, 1, R_256_1, 8 * (R) + 2);                                      \
-  R256(0, 1, 2, 3, R_256_2, 8 * (R) + 3);                                      \
-  R256(0, 3, 2, 1, R_256_3, 8 * (R) + 4);                                      \
-  I256(2 * (R));                                                               \
-  R256(0, 1, 2, 3, R_256_4, 8 * (R) + 5);                                      \
-  R256(0, 3, 2, 1, R_256_5, 8 * (R) + 6);                                      \
-  R256(0, 1, 2, 3, R_256_6, 8 * (R) + 7);                                      \
-  R256(0, 3, 2, 1, R_256_7, 8 * (R) + 8);                                      \
-  I256(2 * (R) + 1);
+#define R512_8_rounds(R) /* do 8 full rounds */                                \
+  R512(0, 1, 2, 3, 4, 5, 6, 7, R_512_0, 8 * (R) + 1);                          \
+  R512(2, 1, 4, 7, 6, 5, 0, 3, R_512_1, 8 * (R) + 2);                          \
+  R512(4, 1, 6, 3, 0, 5, 2, 7, R_512_2, 8 * (R) + 3);                          \
+  R512(6, 1, 0, 7, 2, 5, 4, 3, R_512_3, 8 * (R) + 4);                          \
+  I512(2 * (R));                                                               \
+  R512(0, 1, 2, 3, 4, 5, 6, 7, R_512_4, 8 * (R) + 5);                          \
+  R512(2, 1, 4, 7, 6, 5, 0, 3, R_512_5, 8 * (R) + 6);                          \
+  R512(4, 1, 6, 3, 0, 5, 2, 7, R_512_6, 8 * (R) + 7);                          \
+  R512(6, 1, 0, 7, 2, 5, 4, 3, R_512_7, 8 * (R) + 8);                          \
+  I512(2 * (R) + 1); /* and key injection */
 
-  static const size_t r = 1;
   // 72 rounds
-  R256_8_rounds(0);
-  R256_8_rounds(1);
-  R256_8_rounds(2);
-  R256_8_rounds(3);
-  R256_8_rounds(4);
-  R256_8_rounds(5);
-  R256_8_rounds(6);
-  R256_8_rounds(7);
-  R256_8_rounds(8);
+  R512_8_rounds(0);
+  R512_8_rounds(1);
+  R512_8_rounds(2);
+  R512_8_rounds(3);
+  R512_8_rounds(4);
+  R512_8_rounds(5);
+  R512_8_rounds(6);
+  R512_8_rounds(7);
+  R512_8_rounds(8);
 
   /* do the final "feedforward" xor, update context chaining vars */
   state->chaining[0] = X0 ^ data[0];
   state->chaining[1] = X1 ^ data[1];
   state->chaining[2] = X2 ^ data[2];
   state->chaining[3] = X3 ^ data[3];
+  state->chaining[4] = X4 ^ data[4];
+  state->chaining[5] = X5 ^ data[5];
+  state->chaining[6] = X6 ^ data[6];
+  state->chaining[7] = X7 ^ data[7];
 
   ts[1] &= ~SKEIN_T1_FLAG_FIRST;
 
@@ -136,32 +175,34 @@ void skein_256_process_block(struct skein_256_state *state, uint64_t *data)
 }
 
 /* digest up to len bytes of input (full blocks only) */
-static inline void skein_256_transform(struct skein_256_state *state,
-                                       const uint8_t *data, uint64_t len)
+static inline size_t skein_512_transform(struct skein_512_state *state,
+                                         const uint8_t *const data,
+                                         uint64_t len)
 {
   /* increment block counter */
-  state->block_counter += len / SKEIN_256_BLOCK_SIZE;
-
   /* digest message, one block at a time */
-  for (; len >= SKEIN_256_BLOCK_SIZE;
-       len -= SKEIN_256_BLOCK_SIZE, data += SKEIN_256_BLOCK_SIZE) {
-    skein_256_process_block(state, (uint64_t*)data);
+  const uint8_t *p = data;
+  for (; len > SKEIN_512_BLOCK_SIZE;
+       len -= SKEIN_512_BLOCK_SIZE, p += SKEIN_512_BLOCK_SIZE) {
+    skein_512_process_block(state, (uint64_t *)data, SKEIN_512_BLOCK_SIZE);
   }
+  return (size_t)(p - data);
 }
 
-void skein_256_init(struct skein_256_state *state)
+void skein_512_256_init(struct skein_512_state *state)
 {
-  *state = (struct skein_256_state){
-      .chaining = {0xFC9DA860D048B449, 0x2FCA66479FA7D833, 0xB33BC3896656840F,
-                   0x6A54E920FDE8DA69},
+  *state = (struct skein_512_state){
+      .chaining = {0xCCD044A12FDB3E13, 0xE83590301A79A9EB, 0x55AEA0614F816E6F,
+                   0x2A2767A4AE9B94DB, 0xEC06025E74DD7683, 0xE7A436CDC4746251,
+                   0xC36FBAF9393AD185, 0x3EEDBA1833EDFC13},
       .T = {0, SKEIN_T1_FLAG_FIRST | SKEIN_T1_BLK_TYPE_MSG},
       .buffer = {0},
-      .block_counter = 0,
       .buf_ptr = 0,
       .bits_in_last_byte = 0};
+  state->T[1] = SKEIN_T1_FLAG_FIRST | SKEIN_T1_BLK_TYPE_MSG;
 }
 
-void skein_256_update(struct skein_256_state *state, const void *dataptr,
+void skein_512_update(struct skein_512_state *state, const void *dataptr,
                       size_t databitlen)
 {
   size_t index = 0;
@@ -173,10 +214,10 @@ void skein_256_update(struct skein_256_state *state, const void *dataptr,
   /* if the buffer contains data that has not yet been digested, first
      add data to buffer until full */
   if (state->buf_ptr) {
-    while (state->buf_ptr < SKEIN_256_BLOCK_SIZE && index < msglen) {
+    while (state->buf_ptr < SKEIN_512_BLOCK_SIZE && index < msglen) {
       state->buffer[state->buf_ptr++] = data[index++];
     }
-    if (state->buf_ptr < SKEIN_256_BLOCK_SIZE) {
+    if (state->buf_ptr < SKEIN_512_BLOCK_SIZE) {
       /* buffer still not full, return */
       if (rem) {
         state->bits_in_last_byte = rem;
@@ -185,14 +226,16 @@ void skein_256_update(struct skein_256_state *state, const void *dataptr,
       return;
     }
 
-    /* digest buffer */
-    state->buf_ptr = 0;
-    skein_256_transform(state, state->buffer, SKEIN_256_BLOCK_SIZE);
+    /* digest buffer if there is more data to process */
+    if (index < msglen) {
+      state->buf_ptr = 0;
+      skein_512_transform(state, state->buffer, SKEIN_512_BLOCK_SIZE);
+    }
   }
 
   /* digest bulk of message */
-  skein_256_transform(state, data + index, msglen - index);
-  index += ((msglen - index) / SKEIN_256_BLOCK_SIZE) * SKEIN_256_BLOCK_SIZE;
+
+  index += skein_512_transform(state, data + index, msglen - index);
   /* store remaining data in buffer */
   while (index < msglen) {
     state->buffer[state->buf_ptr++] = data[index++];
@@ -207,7 +250,8 @@ void skein_256_update(struct skein_256_state *state, const void *dataptr,
   }
 }
 
-void skein_256_final(struct skein_256_state *state, uint8_t *digest)
+void skein_512_final(struct skein_512_state *state, size_t digestbitlen,
+                     uint8_t *digest)
 {
   /* tag as the final block */
   state->T[1] |= SKEIN_T1_FLAG_FINAL;
@@ -215,42 +259,41 @@ void skein_256_final(struct skein_256_state *state, uint8_t *digest)
   if (state->bits_in_last_byte) {
     /* handle partial final byte */
     const int BILB = state->bits_in_last_byte;
-     /* partial byte bit mask */
-    uint8_t mask = (uint8_t) (0x1 << (7 - BILB));
+    /* partial byte bit mask */
+    uint8_t mask = (uint8_t)(0x1 << (7 - BILB));
     /* apply bit padding on final byte */
     state->buffer[state->buf_ptr - 1] &= (0 - mask);
     state->buffer[state->buf_ptr - 1] |= mask;
   }
 
-  if(state->buf_ptr > 0) {
-    while(state->buf_ptr < SKEIN_256_BLOCK_SIZE) {
+  if (state->buf_ptr > 0 || state->T[1] & SKEIN_T1_FLAG_FIRST) {
+    size_t bytes_in_buf = state->buf_ptr;
+    while (state->buf_ptr < SKEIN_512_BLOCK_SIZE) {
       state->buffer[state->buf_ptr++] = 0;
     }
     // process final block
-    skein_256_process_block(state, (uint64_t*)state->buffer);
+    skein_512_process_block(state, (uint64_t *)state->buffer, bytes_in_buf);
   }
-
   /* now output the result */
   /* run Threefish in "counter mode" to generate output */
   /* zero out buffer[], so it can hold the counter */
-  memset(state->buffer,0, sizeof(state->buffer));
+  memset(state->buffer, 0, sizeof(state->buffer));
 
   /* build the counter block */
   state->T[0] = 0;
-  state->T[1] = SKEIN_T1_BLK_TYPE_OUT_FINAL;
+  state->T[1] = SKEIN_T1_FLAG_FIRST | SKEIN_T1_BLK_TYPE_OUT_FINAL;
 
   /* run "counter mode" */
-  skein_256_process_block(state, (uint64_t*)state->buffer);
+  skein_512_process_block(state, (uint64_t *)state->buffer, sizeof(uint64_t));
 
   /* "output" the ctr mode bytes */
-  memcpy(digest, state->chaining, SKEIN_256_HASH_BIT_LEN / 8);
-
+  memcpy(digest, state->chaining, digestbitlen / 8);
 }
 
-void skein_256(const void *input, size_t inputbitlen, uint8_t *digest)
+void skein_512_256(const void *input, size_t inputbitlen, uint8_t *digest)
 {
-  struct skein_256_state state;
-  skein_256_init(&state);
-  skein_256_update(&state, input, inputbitlen);
-  skein_256_final(&state, digest);
+  struct skein_512_state state;
+  skein_512_256_init(&state);
+  skein_512_update(&state, input, inputbitlen);
+  skein_512_final(&state, 256, digest);
 }
